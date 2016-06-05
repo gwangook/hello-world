1f9bbc5b0d1bdf89ee238c04415cefd2f42d3c70
gwangook <gwangook@qq.com>
2016-06-06 00:46:46 +0800

    Create HelloWorld.java

---
+++ TestMain/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.ook.TestMain;
+
+public class HelloJava
+{
+	public static void main(String[] args)
+	{
+		System.out.println("hello java");
+        System.out.println("let me see , how to use it ");
+	}
+}
